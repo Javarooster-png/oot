ad3651e8bb79fcb3328ed4a0a9466b097add7487
Javarooster-png <javarooster93@gmail.com>
2023-04-21 22:17:03 -0500

    Removed custom scene and fixed minimap-related pause crash

--- src/code/title_setup.c
+++ src/code/title_setup.c
@@ -3,33 +3,9 @@ void Setup_InitImpl(SetupState* this) {
     osSyncPrintf("ゼルダ共通データ初期化\n"); // "Zelda common data initalization"
     SaveContext_Init();
 
-    /*
     // vanilla
     this->state.running = false;
     SET_NEXT_GAMESTATE(&this->state, ConsoleLogo_Init, ConsoleLogoState);
-    */
-
-    // Load debug save
-    Sram_InitDebugSave();
-
-    // Set age, time and entrance
-    gSaveContext.linkAge = LINK_AGE_ADULT;
-    gSaveContext.dayTime = CLOCK_TIME(12, 0);
-    gSaveContext.entranceIndex = ENTR_MOUNTAIN_VILLAGE_0;
-
-    gSaveContext.respawnFlag = 0;
-    gSaveContext.respawn[RESPAWN_MODE_DOWN].entranceIndex = ENTR_LOAD_OPENING;
-
-    // Have the magic meter load correctly
-    gSaveContext.magicFillTarget = gSaveContext.magic;
-    gSaveContext.magicCapacity = 0;
-    gSaveContext.magic = 0;
-    gSaveContext.magicLevel = 0;
-
-    // Load Play state
-    gSaveContext.gameMode = GAMEMODE_NORMAL;
-    this->state.running = false;
-    SET_NEXT_GAMESTATE(&this->state, Play_Init, PlayState);
 }
 
 void Setup_Destroy(GameState* thisx) {
